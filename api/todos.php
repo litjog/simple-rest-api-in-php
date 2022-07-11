16aea052c4d9f9151555fb566dba8e5d78e567e1
litjog <litjog@gmail.com>
2022-07-11 09:06:12 +0800

    fix unhandled error/exception

--- api/todos.php
+++ api/todos.php
@@ -2,7 +2,17 @@
 
 require_once '../controllers/TodosController.php';
 
-$todosController = new TodosController(true);
-$todosController->handleRequest();
+try {
+  $todosController = new TodosController(true);
+  $todosController->handleRequest();
+} catch (\Throwable $th) {
+  header('Content-Type: application/json');
+  http_response_code(500);
+  echo json_encode(array(
+    'success' => false,
+    'message' => $th->getMessage()
+  ));
+  die();
+}
 
 ?>
